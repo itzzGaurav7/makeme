49ad74611716181647d8b8277b28a545e91579f8
capy-on-caffeine <arnav.anand2004@gmail.com>
2024-11-21 00:03:50 +0530

    sidebar by gpt

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -2,12 +2,16 @@
 import Image from "next/image";
 import { ModeToggle } from "./Component/ModeToggle";
 import DropZone from './Component/DropZone/DropBox'
+import Sidebar from "./Component/Sidebar";
 
 export default function Home() {
   return (
     <div className="relative w-full h-screen">
       <div className="absolute right-3 top-3">
         <ModeToggle/>
+      </div>
+      <div className="absolute top-0 left-0">
+        <Sidebar />
       </div>
       <div className="relative top-10">
         <DropZone/>
